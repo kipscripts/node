8c8bf3c45b0f3c98c3e5cd74c8a59f4f7ace4859
Kip Turk <kipscripts@gmail.com>
2017-11-12 19:39:31 -0700

    Pushing without security files

--- dbsec.js
+++
@@ -1,4 +0,0 @@
-module.exports = {'host': '127.0.0.1',
-         'user': 'dbuser',
-         'password': '@lfab3t!',
-         'database': 'streams'}
--- dbsec.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python34
-
-mysql = {'host': '127.0.0.1',
-         'user': 'dbuser',
-         'pass': '@lfab3t!',
-         'db': 'streams'}
